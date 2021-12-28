0de20528affa3aa7286999e5775335cada986b8a
Maroš Motaj <mmotaj@gmail.com>
2021-12-28 12:44:14 +0100

    Added sql chip check and buzzer

---
+++ DochadzkovySystemProjekt/button.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import RPi.GPIO as GPIO
+
+
+class Button:
+
+    def __init__(self, button_pin):
+        self.button_pin = button_pin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def button_callback(self):
+        while True:
+            if GPIO.input(self.button_pin) == GPIO.HIGH:
+                print("Button pushed")
+            else:
+                print("not pressed")
+
+
+
